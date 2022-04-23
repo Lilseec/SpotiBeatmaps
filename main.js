f46392d9051b351bd41395b3cedc9fd1973505c3
unknown <yura.rossol@gmail.com>
2022-04-23 20:35:14 +0300

    true

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 const authorizeBtn = document.getElementById('authorization')
 
-function authorization() {
+async function authorization() {
         let idx = window.location.href.lastIndexOf('?code=')
         if (window.location.href.includes('?code')) {
-            let idx = window.location.href.lastIndexOf('?code=')
-            console.log(idx)
+            let url = ' https://osu.ppy.sh/oauth/token'
+
+            let idx = window.location.href.lastIndexOf('?code=') + 6
+            let code = window.location.href.slice(idx)
+
+            let data = {
+                'client_id': '14338',
+                'client_secret': 'lnB9AtwwoREgoAatHBnpDbFr93KabxwyQ2CPac0h',
+                'code': code,
+                'grant_type': 'authorization_code',
+                'redirect_uri': 'https://lilseec.github.io/SpotiBeatmaps'
+            }
+            
+            const response = await fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(data)
+            })
+            const json = await response.json()
+            console.log('Успех:', json);
         } else {
             window.location.replace('https://osu.ppy.sh/oauth/authorize?response_type=code&client_id=14338&redirect_uri=https://lilseec.github.io/SpotiBeatmaps')
     }
