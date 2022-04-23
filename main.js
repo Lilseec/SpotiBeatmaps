3b3eebcd063b40dbc8087212ffb317a4c9bd9de6
unknown <yura.rossol@gmail.com>
2022-04-23 20:38:00 +0300

    false

--- main.js
+++ main.js
@@ -18,7 +18,10 @@ async function authorization() {
             
             const response = await fetch(url, {
                 method: 'POST',
-                body: JSON.stringify(data)
+                body: JSON.stringify(data),
+                headers: {
+                    'Access-Control-Allow-Origin': '*'
+                }
             })
             const json = await response.json()
             console.log('Успех:', json);
