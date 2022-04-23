70d1439bc8fc4c2219fbe86aae91fdc89d02182b
unknown <yura.rossol@gmail.com>
2022-04-23 20:13:53 +0300

    ahaahahaha

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const authorizeBtn = document.getElementById('authorization')
 
 function authorization() {
         let idx = window.location.href.lastIndexOf('?code=')
-        if (window.location.includes('?code')) {
-        let idx = window.location.href.lastIndexOf('?code=')
-        console.log(idx)
-    } else {
-        window.location.replace('https://osu.ppy.sh/oauth/authorize?response_type=code&client_id=14338&redirect_uri=https://lilseec.github.io/SpotiBeatmaps')
+        if (window.location.href.includes('?code')) {
+            let idx = window.location.href.lastIndexOf('?code=')
+            console.log(idx)
+        } else {
+            window.location.replace('https://osu.ppy.sh/oauth/authorize?response_type=code&client_id=14338&redirect_uri=https://lilseec.github.io/SpotiBeatmaps')
     }
 }
 
