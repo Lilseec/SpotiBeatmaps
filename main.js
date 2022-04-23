3845b9cfd7fcc8a3a3be4a9adccfc308da5db17f
unknown <yura.rossol@gmail.com>
2022-04-23 20:09:56 +0300

    gay

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const authorizeBtn = document.getElementById('authorization')
 
 function authorization() {
-    if (window.location.includes('?code')) {
-        let idx = window.location.lastIndexOf('?code=')
+        let idx = window.location.href.lastIndexOf('?code=')
+        if (window.location.includes('?code')) {
+        let idx = window.location.href.lastIndexOf('?code=')
         console.log(idx)
     } else {
         window.location.replace('https://osu.ppy.sh/oauth/authorize?response_type=code&client_id=14338&redirect_uri=https://lilseec.github.io/SpotiBeatmaps')
