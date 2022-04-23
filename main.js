e00c57295cd6f904b01f925f673a877478be8811
unknown <yura.rossol@gmail.com>
2022-04-23 23:58:48 +0300

    switch to client credentials

--- main.js
+++ main.js
@@ -1,34 +1,26 @@
 const authorizeBtn = document.getElementById('authorization')
 
 async function authorization() {
-        let idx = window.location.href.lastIndexOf('?code=')
-        if (window.location.href.includes('?')) {
-            let url = ' https://osu.ppy.sh/oauth/token'
+        let url = ' https://osu.ppy.sh/oauth/token'
 
-            let idx = window.location.href.lastIndexOf('?code=') + 6
-            let code = window.location.href.slice(idx)
-            console.log('Test:', code)
-            let data = {
-                "code": code,
-                "client_id": 14338,
-                "client_secret": "lnB9AtwwoREgoAatHBnpDbFr93KabxwyQ2CPac0h",
-                "redirect_uri": "https://lilseec.github.io/SpotiBeatmaps",
-                'grant_type': "authorization_code"
-            }
+        let data = {
+            "client_id": 14338,
+            "client_secret": "lnB9AtwwoREgoAatHBnpDbFr93KabxwyQ2CPac0h",
+            "grant_type": "client_credentials",
+            "scope": "public"
+        }
             
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    "Accept": "application/json",
-                    "Content-Type": "application/x-www-form-urlencoded"
-                },
-                body: JSON.stringify(data)
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
             })
-            const json = await response.json()
-            console.log('Успех:', json);
-        } else {
-            window.location.replace('https://osu.ppy.sh/oauth/authorize?response_type=code&client_id=14338&redirect_uri=https://lilseec.github.io/SpotiBeatmaps')
-    }
+        const json = await response.json()
+        console.log('Успех:', json);
+        
 }
 
 window.addEventListener('load', authorization)
